265c526f5393d3e051d4f9d9850e75a52a377975
Ivan Lozovyi <vebster2580@gmail.com>
2024-06-24 20:17:50 +0300

    Add and Create: MVC pattern and documents(router and controller)

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Movie = require('./models/movie');
+const movieRouter = require('./routes/movie-routes');
 
 const PORT = 3000;
 const URL = 'mongodb://localhost:27017/mongoPractise';
 
 const app = express();
 app.use(express.json());
+app.use(movieRouter);
 
 mongoose
     .connect(URL)
@@ -16,65 +18,3 @@ mongoose
 app.listen(PORT, (err) => {
     err ? console.log(err) : console.log(`Listening on port ${PORT}`)
 });
-
-const handleError = (res, error) => {
-    res.status(500).json({error});
-}
-
-app.get('/movies', (req, res) => {
-    Movie
-        .find()
-        .sort({title: 1})
-        .then((movies) => {
-            res
-                .status(200)
-                .json(movies);
-        })
-        .catch(() => handleError(res, "Something goes wrong..."))
-});
-
-app.get('/movies/:id', (req, res) => {
-    Movie
-        .findById(req.params.id)
-        .then((movie) => {
-            res
-                .status(200)
-                .json(movie);
-        })
-        .catch(() => handleError(res, "Something goes wrong..."))
-})
-
-app.delete('/movies/:id', (req, res) => {
-    Movie
-        .findByIdAndDelete(req.params.id)
-        .then((result) => {
-            res
-                .status(200)
-                .json(result);
-        })
-        .catch(() => handleError(res, "Something goes wrong..."))
-});
-
-app.post('/movies', (req, res) => {
-    const movie = new Movie(req.body)
-
-    movie
-        .save()
-        .then((result) => {
-            res
-                .status(201)
-                .json(result);
-        })
-        .catch(() => handleError(res, "Something goes wrong..."));
-});
-
-app.patch('/movies/:id', (req, res) => {
-    Movie
-        .findByIdAndUpdate(req.params.id, req.body) 
-        .then((result) => {
-            res
-                .status(200)
-                .json(result);
-        })
-        .catch((err) => handleError(res, "Something goes wrong..."))
-})
